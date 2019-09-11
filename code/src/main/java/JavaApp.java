5051fab4d07f6ae813466c9a84144e470f08b7d4
James Ward <james@jamesward.com>
2019-09-11 13:48:33 -0600

    back to start

--- code/src/main/java/JavaApp.java
+++ code/src/main/java/JavaApp.java
@@ -14,17 +14,14 @@ import static stuff.JavaStuff.*;
 class JavaApp {
 
     public static void main(String[] args) {
-        Function<String, Integer> count = String::length;
+        var a = addOne(2);
 
-        var result = ws.apply(client -> {
-            Supplier<CompletableFuture<String>> randomWord = () -> client.url(randomWordUrl).get().thenApply(StandaloneWSResponse::getBody).toCompletableFuture();
+        var b = addOne(2);
 
-            return client.url(randomNumUrl).get().thenCompose( response -> {
-                var num = Integer.parseInt(response.getBody());
-                return sequence(randomWord, num).thenApply(words -> String.join(" ", words));
-            });
-        });
+        var c = addOne(2);
 
-        result.thenRun(System.out::println);
+        var d = addOne(2);
+
+        var e = addOne(2);
     }
 }
